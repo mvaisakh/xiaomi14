a62b9a4d6931e7e71ba182201c9c2481477f6ab6
Vaisakh Murali <mvaisakh@statixos.com>
2025-03-24 00:04:40 +0530

    securemsm: fix trace include paths for in-tree builds

--- securemsm-kernel/smcinvoke/trace_smcinvoke.h
+++ securemsm-kernel/smcinvoke/trace_smcinvoke.h
@@ -492,7 +492,7 @@ TRACE_EVENT(smcinvoke_release,
 * Define path if not defined in bazel file
  */
 #ifndef SMCINVOKE_TRACE_INCLUDE_PATH
-#define SMCINVOKE_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/securemsm-kernel/smcinvoke
+#define SMCINVOKE_TRACE_INCLUDE_PATH ../../securemsm-kernel/smcinvoke
 #endif
 
 #undef TRACE_INCLUDE_PATH
