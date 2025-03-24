444ab5e4180cb2dae0133dcba4497e2c3acbbf4c
Vaisakh Murali <mvaisakh@statixos.com>
2025-03-24 17:35:34 +0530

    dsp: fix trace include path for in-kernel builds

--- dsp/dsp/fastrpc_trace.h
+++ dsp/dsp/fastrpc_trace.h
@@ -15,7 +15,7 @@
  * Define path if not defined in bazel file
  */
 #ifndef DSP_TRACE_INCLUDE_PATH
-#define DSP_TRACE_INCLUDE_PATH ../../../../vendor/qcom/opensource/dsp-kernel/dsp
+#define DSP_TRACE_INCLUDE_PATH ../../dsp/dsp
 #endif
 
 #undef TRACE_INCLUDE_PATH
