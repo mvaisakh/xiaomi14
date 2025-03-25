494bfd962f13c1adf4ad81afe92a9cf4169f9355
Vaisakh Murali <mvaisakh@statixos.com>
2025-03-25 17:48:03 +0530

    drivers/power: import OEM changes
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- drivers/power/reset/qcom-reboot-reason.c
+++ drivers/power/reset/qcom-reboot-reason.c
@@ -17,10 +17,12 @@
 #include <linux/nvmem-consumer.h>
 #include <linux/of_device.h>
 #include <linux/of_platform.h>
+#include <linux/panic_notifier.h>
 
 struct qcom_reboot_reason {
 	struct device *dev;
 	struct notifier_block reboot_nb;
+	struct notifier_block panic_nb;
 	struct nvmem_cell *nvmem_cell;
 	struct poweroff_reason *reasons;
 };
@@ -38,6 +40,9 @@ static struct poweroff_reason pon_reasons[] = {
 	{ "dm-verity device corrupted",	0x04, 0x1 },
 	{ "dm-verity enforcing",	0x05, 0x1 },
 	{ "keys clear",			0x06, 0x1 },
+        { "ffu",                        0x41, 0x1 },
+        { "panic",                      0x21, 0x1 },
+        { NULL,                         0x20, 0x1 },
 	{}
 };
 
@@ -58,6 +63,47 @@ static const struct of_device_id of_qcom_reboot_reason_match[] = {
 };
 MODULE_DEVICE_TABLE(of, of_qcom_reboot_reason_match);
 
+#define RESTART_REASON_PANIC  7
+#define RESTART_REASON_NORMAL 8
+#define RESTART_REASON_EIO 3
+
+static struct qcom_reboot_reason *ffu_reboot = NULL;
+
+int ufs_ffu_reboot_reason_reboot(void *ptr)
+{
+	char *cmd = ptr;
+	struct qcom_reboot_reason *reboot;
+	struct poweroff_reason *reason;
+
+	if(!ffu_reboot)
+		return NOTIFY_BAD;
+
+	reboot = ffu_reboot;
+
+	if (!cmd) {
+		nvmem_cell_write(reboot->nvmem_cell,
+				 &reasons[RESTART_REASON_NORMAL].pon_reason,
+				 sizeof(reasons[RESTART_REASON_NORMAL].pon_reason));
+		return NOTIFY_OK;
+	}
+
+	for (reason = reasons; reason->cmd; reason++) {
+		if (!strcmp(cmd, reason->cmd)) {
+			nvmem_cell_write(reboot->nvmem_cell,
+					 &reason->pon_reason,
+					 sizeof(reason->pon_reason));
+			return NOTIFY_OK;
+		}
+	}
+
+	nvmem_cell_write(reboot->nvmem_cell,
+			&reason->pon_reason,
+			sizeof(reason->pon_reason));
+
+	return NOTIFY_OK;
+}
+EXPORT_SYMBOL(ufs_ffu_reboot_reason_reboot);
+
 static int qcom_reboot_reason_reboot(struct notifier_block *this,
 				     unsigned long event, void *ptr)
 {
@@ -67,7 +113,10 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 		struct qcom_reboot_reason, reboot_nb);
 	struct poweroff_reason *reason;
 
-	if (!cmd)
+	if (!cmd) {
+		nvmem_cell_write(reboot->nvmem_cell,
+				 &reasons[RESTART_REASON_NORMAL].pon_reason,
+				 sizeof(reasons[RESTART_REASON_NORMAL].pon_reason));
 		return NOTIFY_OK;
 	for (reason = reboot->reasons; reason->cmd; reason++) {
 		if (!strcmp(cmd, reason->cmd)) {
@@ -79,8 +128,31 @@ static int qcom_reboot_reason_reboot(struct notifier_block *this,
 			break;
 		}
 	}
+	nvmem_cell_write(reboot->nvmem_cell,
+			&reason->pon_reason,
+			sizeof(reason->pon_reason));
 
 	return NOTIFY_OK;
+}
+
+static int panic_prep_restart(struct notifier_block *this,
+			      unsigned long event, void *ptr)
+{
+	struct qcom_reboot_reason *reboot = container_of(this,
+		struct qcom_reboot_reason, panic_nb);
+	char *cmd = ptr;
+	/* BSP-Kernel@Xiaomi add for EIO mode */
+	if(!strcmp("dm-verity device corrupted",cmd)) {
+		nvmem_cell_write(reboot->nvmem_cell,
+				&reasons[RESTART_REASON_EIO].pon_reason,
+				sizeof(reasons[RESTART_REASON_EIO].pon_reason));
+	}
+	else {
+		nvmem_cell_write(reboot->nvmem_cell,
+				&reasons[RESTART_REASON_PANIC].pon_reason,
+				sizeof(reasons[RESTART_REASON_PANIC].pon_reason));
+	}
+	return NOTIFY_DONE;
 }
 
 static int qcom_reboot_reason_probe(struct platform_device *pdev)
@@ -116,6 +188,12 @@ static int qcom_reboot_reason_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, reboot);
 
+	reboot->panic_nb.notifier_call = panic_prep_restart;
+	reboot->panic_nb.priority = INT_MAX;
+	atomic_notifier_chain_register(&panic_notifier_list, &reboot->panic_nb);
+
+	ffu_reboot = reboot;
+
 	return 0;
 }
 
@@ -123,6 +201,7 @@ static int qcom_reboot_reason_remove(struct platform_device *pdev)
 {
 	struct qcom_reboot_reason *reboot = platform_get_drvdata(pdev);
 
+	atomic_notifier_chain_unregister(&panic_notifier_list, &reboot->panic_nb);
 	unregister_reboot_notifier(&reboot->reboot_nb);
 
 	return 0;
